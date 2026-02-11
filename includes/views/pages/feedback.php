3bda8bc4630e65712a9ae9d4f78218eb694394c0
fariborzj2 <fariborzj2@gmail.com>
2026-02-11 16:51:38 +0330

    fix

--- includes/views/pages/feedback.php
+++ includes/views/pages/feedback.php
@@ -1,6 +1,6 @@
 <div class="section">
     <div class="bg-block pd-md border basis-250 grow-1 radius-16">
-        <div class="border-bottom bg-block pd-10">
+        <div class="border-bottom bg-block pd-1 mb-2">
             <h2 class="font-size-6 font-black text-title">تماس با ما</h2>
             <p class="text-gray font-size-2">نظرات، پیشنهادات و مشکلات خود را با ما در میان بگذارید.</p>
         </div>
