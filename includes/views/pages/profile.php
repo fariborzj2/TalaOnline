963e0c146303119a80c7502bd716e9a4f08ebe3f
fariborzj2 <fariborzj2@gmail.com>
2026-02-21 19:04:14 +0330

    fix

--- includes/views/pages/profile.php
+++ includes/views/pages/profile.php
@@ -15,7 +15,7 @@
         </div>
         <div class="grow-1">
             <h1 class="font-size-4 font-black text-title"><?= htmlspecialchars($_SESSION['user_name']) ?></h1>
-            <p class="text-gray font-size-1 ltr text-tight mt-05">
+            <p class="text-gray font-size-1 ltr text-right mt-05">
                 <?= htmlspecialchars($_SESSION['user_email']) ?>
                 <span class="mx-1">|</span>
                 <span class="bg-secondary px-1 radius-8 border">@<?= htmlspecialchars($_SESSION['user_username'] ?? '---') ?></span>
